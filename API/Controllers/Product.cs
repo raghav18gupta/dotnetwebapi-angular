305f81c71e29c10e4972059b860fdc385e4466f6
raghav18gupta <18raghavgupta@gmail.com>
2021-12-03 16:58:29 +0530

    Section 1 - API Basic

---
+++ API/Controllers/Product.cs
@@ -0,0 +1,29 @@
+﻿using Infrastructure.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class Product : Controller
+    {
+        private readonly StoreContext _context;
+        public Product(StoreContext storeContext)
+        {
+            _context = storeContext;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Product>>> GetProducts()
+        {
+            return Ok(await _context.Products.ToListAsync());
+        }
+
+        [HttpGet("{i}")]
+        public async Task<ActionResult<Product>> GetProduct(int i)
+        {
+            return Ok(await _context.Products.FindAsync(i));
+        }
+    }
+}
